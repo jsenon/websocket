d39f288e9cc973e1fa04ecf8adfec8996031db92
Julien SENON <julien.senon@gmail.com>
2017-03-16 15:43:38 +0100

    add auth

--- src/main.go
+++ src/main.go
@@ -34,7 +34,9 @@ func main() {
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	// http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+
 	http.Handle("/room", r)
 
 	// get the room going
